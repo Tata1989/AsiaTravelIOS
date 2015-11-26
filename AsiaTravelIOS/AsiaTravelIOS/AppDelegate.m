5fdad498c91a2cc903f9e6db7c1783d81ff22a40
wangxinxin <wangxinxin@asiatravel.com>
2015-11-26 16:47:00 +0800

    添加地图定位

--- AsiaTravelIOS/AsiaTravelIOS/AppDelegate.m
+++ AsiaTravelIOS/AsiaTravelIOS/AppDelegate.m
@@ -9,9 +9,12 @@
 #import "AppDelegate.h"
 #import "ATNavigationController.h"
 #import "ATTabBarController.h"
+#import <CoreLocation/CoreLocation.h>
+#import <CoreLocation/CLLocationManagerDelegate.h>
 
-@interface AppDelegate ()
 
+@interface AppDelegate ()<CLLocationManagerDelegate>
+@property(nonatomic,strong)CLLocationManager *locationManager;
 @end
 
 @implementation AppDelegate
@@ -33,9 +36,70 @@
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     
+    [self getLocation];
+    
     return YES;
 }
+/**
+ *  获取用户位置
+ */
+- (void)getLocation
+{
+ 
+    if ([CLLocationManager locationServicesEnabled]) {
+        if (!_locationManager) {
+            _locationManager = [[CLLocationManager alloc] init];
+            _locationManager.delegate = self;
+            _locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters;
+            _locationManager.distanceFilter = kCLDistanceFilterNone;
+            if ([_locationManager respondsToSelector:@selector(requestAlwaysAuthorization)]) {
+                [_locationManager requestAlwaysAuthorization];
+            }
+            if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 8.0)//6
+                [_locationManager requestWhenInUseAuthorization];
+            [_locationManager startUpdatingLocation];
+        }
+    }
+}
+#pragma mark - CLLocationManagerDelegate
 
+-(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations{
+
+    CLLocation *cloc = [locations lastObject];
+    
+    CLGeocoder * geoCoder = [[CLGeocoder alloc] init];
+    [geoCoder reverseGeocodeLocation:cloc completionHandler:^(NSArray *placemarks, NSError *error) {
+        if (!error){
+            for (CLPlacemark * placemark in placemarks) {
+                
+                NSDictionary *dic = [placemark addressDictionary];
+                //  Country(国家)  State(城市)  SubLocality(区)
+                if (![[[NSUserDefaults standardUserDefaults] objectForKey:@"gpsCity"] isEqualToString:[dic objectForKey:@"State"]]) {
+                    DDLog(@"城市：%@ 区：%@",[dic objectForKey:@"State"],[dic objectForKey:@"SubLocality"]);
+                    
+                    [[NSUserDefaults standardUserDefaults] setObject:[dic objectForKey:@"State"] forKey:@"gpsCity"];
+                    
+                    [[NSUserDefaults standardUserDefaults] setObject:[dic objectForKey:@"SubLocality"] forKey:@"gpsSubLocality"];
+                    
+                    [[NSUserDefaults standardUserDefaults] synchronize];
+                }
+            
+            }
+        }
+        [_locationManager stopUpdatingLocation];
+    }];
+    
+    CGFloat latitude = manager.location.coordinate.latitude;
+    NSString *latitudeStr = [NSString stringWithFormat:@"%f",latitude];
+    CGFloat longitude = manager.location.coordinate.longitude;
+    NSString *longitudeStr = [NSString stringWithFormat:@"%f",longitude];
+    
+//    [[NSUserDefaults standardUserDefaults] setObject:latitudeStr forKey:@"latitude"];
+//    [[NSUserDefaults standardUserDefaults] setObject:longitudeStr forKey:@"longitude"];
+//    [[NSUserDefaults standardUserDefaults] synchronize];
+    DDLog(@"%@--%@",latitudeStr,longitudeStr);
+    
+}
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
